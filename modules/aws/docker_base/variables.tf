1de2c07f95da7bdb4070cb43a780b1f1cae2bc53
Brandon garcia <bgar@Brandons-MacBook-Pro.local>
2021-04-06 11:57:15 -0500

    Adding description

--- modules/aws/docker_base/variables.tf
+++ modules/aws/docker_base/variables.tf
@@ -3,14 +3,17 @@
 #******************
 variable "env" {
   type = string
+  description = "The environment tag to be used with the ec2 instance"
 }
 
 variable "contact" {
   type = string
+  description = "The contact tag to be used with the ec2 instance"
 }
 
 variable "project" {
   type = string
+  description = "The project tag to be used with the ec2 instance"
 }
 
 #*******************
@@ -18,8 +21,10 @@ variable "project" {
 #*******************
 variable "vpc_id" {
   type = string
+  description = "The VPC ID for the EC2 instance to be deployed into"
 }
 
 variable "instance_type" {
   type = string
+  description = "The instance type of the EC2 instance"
 }
